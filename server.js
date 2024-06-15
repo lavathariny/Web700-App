7ad71137077588a9d110afe84f3f5579c1b371a9
Lavatharini <134617779+lavathariny@users.noreply.github.com>
2024-06-14 22:40:02 -0400

    Add files via upload

---
+++ server.js
@@ -0,0 +1,93 @@
+
+/*********************************************************************************
+*  WEB700 – Assignment 03
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part 
+*  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: Lavatharini Jasinthakumar Student ID: 153494232 Date: 14 June 2024
+*
+********************************************************************************/ 
+
+
+var HTTP_PORT = process.env.PORT || 8080;
+var express = require("express");
+var path = require("path");
+var collegeData = require("./modules/collegeData");
+var app = express();
+
+// Serve static files from the 'views' directory
+app.use(express.static('views'));
+
+// GET route to return the home.html file
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/home.html"));
+});
+
+// GET route to return the about.html file
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/about.html"));
+});
+
+// GET route to return the htmlDemo.html file
+app.get("/htmlDemo", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/htmlDemo.html"));
+});
+
+// GET route to return all students or students by course
+app.get("/students", (req, res) => {
+    if (req.query.course) {
+        collegeData.getStudentsByCourse(req.query.course).then((data) => {
+            res.json(data);
+        }).catch((err) => {
+            res.json({ message: "no results" });
+        });
+    } else {
+        collegeData.getAllStudents().then((data) => {
+            res.json(data);
+        }).catch((err) => {
+            res.json({ message: "no results" });
+        });
+    }
+});
+
+// GET route to return all TAs
+app.get("/tas", (req, res) => {
+    collegeData.getTAs().then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json({ message: "no results" });
+    });
+});
+
+// GET route to return all courses
+app.get("/courses", (req, res) => {
+    collegeData.getCourses().then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json({ message: "no results" });
+    });
+});
+
+// GET route to return a student by student number
+app.get("/student/:num", (req, res) => {
+    collegeData.getStudentByNum(req.params.num).then((data) => {
+        res.json(data);
+    }).catch((err) => {
+        res.json({ message: "no results" });
+    });
+});
+
+// Handle 404 errors
+app.use((req, res) => {
+    res.status(404).send("Page Not THERE, Are you sure of the path?");
+});
+
+// Initialize the data and start the server
+collegeData.initialize().then(() => {
+    app.listen(HTTP_PORT, () => {
+        console.log("server listening on port: " + HTTP_PORT);
+    });
+}).catch((err) => {
+    console.error("unable to start server: " + err);
+});
